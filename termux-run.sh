c49f0c50171fdbbcda0ec3664bd987ae63bcb8f8
Ken-mwaniki1 <kenmwaniki082@gmail.com>
2024-11-30 23:00:29 +0300

    Termux option added

---
+++ termux-run.sh
@@ -0,0 +1,31 @@
+termux-setup-storage  # Corrected command for setting up storage
+pkg update && pkg upgrade -y
+pkg install python python3-venv mariadb
+
+# Set environment variables for proper compilation
+export LDFLAGS="-L/data/data/com.termux/files/usr/lib"
+export CFLAGS="-I/data/data/com.termux/files/usr/include"
+
+# Install required libraries for image processing (Pillow dependencies)
+pkg install libjpeg-turbo libpng zlib
+
+# Set up Python virtual environment
+python -m venv ~/.venv
+source ~/.venv/bin/activate
+
+#cd to phone root
+cd storage/shared
+
+git clone https://github.com/Ken-mwaniki1/Hotel_Way Hotel_Way
+cd Hotel_Way
+
+# Install project dependencies
+pip install -r requirements.txt 
+
+# Install additional dependencies separately
+pip install wheel pymysql mysqlclient tzdata
+
+# Run Django commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
